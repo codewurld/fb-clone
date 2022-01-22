7789b3157b290c76e75f3b9714cad192d6344b34
codewurld <sholaq@gmail.com>
2022-01-22 23:13:11 +0000

    notification added to landing page

--- src/components/login/Login2.jsx
+++ src/components/login/Login2.jsx
@@ -9,7 +9,7 @@ import { actionTypes } from '../../reducer';
 import { useStateValue } from '../../ContextProvider';
 import { Link } from 'react-router-dom';
 
-
+// temporary login component for demo rendering
 const Login = () => {
 
     const [state, dispatch] = useStateValue();
@@ -34,7 +34,7 @@ const Login = () => {
                 <img src={Facebook} alt="Facebook Logo" className="logo1" />
 
                 <p>This is a clone demo site built for the purpose of learning. <br />
-                    Login does not require an email address.
+                    Login does not require an email address.<br /> [Web version: best viewed on a computer/laptop.]
                 </p>
 
             </div>
